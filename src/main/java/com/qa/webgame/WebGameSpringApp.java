349f421b2c1999a129c5080ef00fc54308a895ca
ConwayQA <lukeconwayqa@googlemail.com>
2020-05-07 14:39:15 +0100

    Created project and set up pom.xml

---
+++ src/main/java/com/qa/webgame/WebGameSpringApp.java
@@ -0,0 +1,13 @@
+package com.qa.webgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebGameSpringApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebGameSpringApp.class, args);
+	}
+
+}
---
+++ src/test/java/com/qa/webgame/WebGameSpringAppTests.java
@@ -0,0 +1,13 @@
+package com.qa.webgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebGameSpringAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
