af240594bb61ead1b080216a0d5395b4602b7375
ConwayQA <lukeconwayqa@googlemail.com>
2020-05-07 15:42:45 +0100

    added item not found exception

---
+++ src/main/java/com/qa/webgame/exceptions/ItemNotFoundException.java
@@ -0,0 +1,15 @@
+package com.qa.webgame.exceptions;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "The item does not exist")
+public class ItemNotFoundException extends EntityNotFoundException {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+}
