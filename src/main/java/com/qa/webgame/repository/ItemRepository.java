35cd6d705cd7c81c64d42b5ddd0e62b8ecbfd2c4
ConwayQA <lukeconwayqa@googlemail.com>
2020-05-07 15:40:16 +0100

    added item repository

---
+++ src/main/java/com/qa/webgame/repository/ItemRepository.java
@@ -0,0 +1,11 @@
+package com.qa.webgame.repository;
+
+import com.qa.webgame.domain.Item;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Long>{
+    
+}
