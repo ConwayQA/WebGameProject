52a2319d30d023f70657a8f6ad0e4450494856e6
ConwayQA <lukeconwayqa@googlemail.com>
2020-05-07 15:48:48 +0100

    added item dto

---
+++ src/main/java/com/qa/webgame/dto/ItemDTO.java
@@ -0,0 +1,112 @@
+package com.qa.webgame.dto;
+
+public class ItemDTO {
+    
+    private Long itemId;
+    private Long rarity;
+    private Double chance;
+    private String description;
+    private String properties;
+
+    public ItemDTO() {
+    }
+
+    public ItemDTO(Long rarity, Double chance, String description, String properties) {
+        this.rarity = rarity;
+        this.chance = chance;
+        this.description = description;
+        this.properties = properties;
+    }
+
+    public Long getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Long itemId) {
+        this.itemId = itemId;
+    }
+
+    public Long getRarity() {
+        return rarity;
+    }
+
+    public void setRarity(Long rarity) {
+        this.rarity = rarity;
+    }
+
+    public Double getChance() {
+        return chance;
+    }
+
+    public void setChance(Double chance) {
+        this.chance = chance;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getProperties() {
+        return properties;
+    }
+
+    public void setProperties(String properties) {
+        this.properties = properties;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((chance == null) ? 0 : chance.hashCode());
+        result = prime * result + ((description == null) ? 0 : description.hashCode());
+        result = prime * result + ((itemId == null) ? 0 : itemId.hashCode());
+        result = prime * result + ((properties == null) ? 0 : properties.hashCode());
+        result = prime * result + ((rarity == null) ? 0 : rarity.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ItemDTO other = (ItemDTO) obj;
+        if (chance == null) {
+            if (other.chance != null)
+                return false;
+        } else if (!chance.equals(other.chance))
+            return false;
+        if (description == null) {
+            if (other.description != null)
+                return false;
+        } else if (!description.equals(other.description))
+            return false;
+        if (itemId == null) {
+            if (other.itemId != null)
+                return false;
+        } else if (!itemId.equals(other.itemId))
+            return false;
+        if (properties == null) {
+            if (other.properties != null)
+                return false;
+        } else if (!properties.equals(other.properties))
+            return false;
+        if (rarity == null) {
+            if (other.rarity != null)
+                return false;
+        } else if (!rarity.equals(other.rarity))
+            return false;
+        return true;
+    }
+
+
+    
+}
