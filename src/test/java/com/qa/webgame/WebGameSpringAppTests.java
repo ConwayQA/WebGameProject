74f5adaa4d6b2408c8c83b0b700f8467530c5bec
ConwayQA <lukeconwayqa@googlemail.com>
2020-05-07 16:01:08 +0100

    quick edit to spring app test

--- src/test/java/com/qa/webgame/WebGameSpringAppTests.java
+++ src/test/java/com/qa/webgame/WebGameSpringAppTests.java
@@ -1,5 +1,7 @@
 package com.qa.webgame;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +9,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 class WebGameSpringAppTests {
 
 	@Test
-	void contextLoads() {
+	public void shouldAnswerWithTrue(){
+		WebGameSpringApp app = new WebGameSpringApp();
+		String[] args = null;
+		WebGameSpringApp.main(args);
+		assertTrue(app instanceof WebGameSpringApp);
 	}
 
 }
